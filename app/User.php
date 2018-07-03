92e4eb4390d47a226ff80e15e8933495205d1341
hungju0113 <40749543+hungju0113@users.noreply.github.com>
2018-07-03 10:44:30 +0800

    安裝後台的使用者角色管理

--- app/User.php
+++ app/User.php
@@ -1,13 +1,13 @@
-<?php
+<?php namespace App;
 
-namespace App;
-
-use Illuminate\Notifications\Notifiable;
+use Backpack\CRUD\CrudTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use CrudTrait;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
